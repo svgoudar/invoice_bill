c53793246792b3d0b825f1f7c4fb9ad2bb7ce3a3
svgoudar <48946832+svgoudar@users.noreply.github.com>
2022-01-16 22:36:10 +0530

    Update settings.py

--- api_store/settings.py
+++ api_store/settings.py
@@ -119,8 +119,11 @@ LOGIN_REDIRECT_URL = 'purchase'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR , 'static_cdn') 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "apistore_app","static")                # mention folders in saticfiles_dirs where django has to look for static files except static folder inside apps during development 
+] 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
