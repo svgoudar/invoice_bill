86ffea0f61b88ace4e8bc8d07ef34b3a624438ce
svgoudar <48946832+svgoudar@users.noreply.github.com>
2022-01-16 19:12:01 +0530

    Delete api_store/apistore_app directory

--- api_store/apistore_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import items
-# Register your models here.
-from django.contrib import admin
-
-
-admin.site.register(items)
-# class PostModelAdmin(admin.ModelAdmin):
-#     list_display = ["purchased_date", "item", "item_category", "quantity", "price"]
