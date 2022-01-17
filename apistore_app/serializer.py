cec81d34f643d39a08fc1009bfb069948bb80ccf
svgoudar <48946832+svgoudar@users.noreply.github.com>
2022-01-17 22:35:03 +0530

    Update serializer.py

--- apistore_app/serializer.py
+++ apistore_app/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers, viewsets, decorators
 from .models import items
-
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class ItemtSerializer(serializers.ModelSerializer):
     class Meta:
